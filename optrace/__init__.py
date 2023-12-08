92fff7f0df856016b0a43fffbe9ee820e3f8b7b6
Damian Mendroch <damian.mendroch@th-koeln.de>
2023-12-08 15:47:07 +0100

    Possible fix 3 for OS Test on Windows failing on example

--- optrace/__init__.py
+++ optrace/__init__.py
@@ -6,7 +6,8 @@ from .__metadata__ import *
 import warnings
 
 def warning_on_one_line(message, category, filename, lineno, file=None, line=None):
-    return "Warning: " + str(message) + "\n" 
+
+    return source + ": Warning: " + str(message) + "\n" 
     # return ' %s:%s: %s:%s\n' % (filename, lineno, category.__name__, message)
 
 warnings.formatwarning = warning_on_one_line
