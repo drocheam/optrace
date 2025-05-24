Fixed calculation of bending direction.

optrace/tracer/geometry/surface/ring_surface.py
@@ -87,13 +87,13 @@ class RingSurface(Surface):
         return X+self.pos[0], Y+self.pos[1], Z
 
     def hurb_props(self, x: np.ndarray, y: np.ndarray)\
-            -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+            -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         """
         Calculates the properties for Heisenberg Uncertainty Ray Bending.
 
         :param x: ray position at surface, x-coordinate
         :param y: ray position at surface, y-coordinate
-        :return: distances axis 1, distances axis 2, axis 1 vector, axis 2 vector, ray mask for rays to bend
+        :return: distances axis 1, distances axis 2, minor axis vector, ray mask for rays to bend
         """
         # polar coordinates
         r = np.sqrt((x - self.pos[0])**2 + (y - self.pos[1])**2)
@@ -115,13 +115,8 @@ class RingSurface(Surface):
         b = np.zeros((b_.shape[0], 3))
         b[:, 0] = np.cos(theta[inside]) 
         b[:, 1] = np.sin(theta[inside]) 
-        
-        # ellipse major axis as vector
-        a = np.zeros_like(b)
-        a[:, 0] = -b[:, 1] 
-        a[:, 1] =  b[:, 0]
 
-        return a_, b_, a, b, inside
+        return a_, b_, b, inside
 
     def mask(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
         """

optrace/tracer/geometry/surface/slit_surface.py
@@ -69,34 +69,29 @@ class SlitSurface(RectangularSurface):
         return X, Y, Z
     
     def hurb_props(self, x: np.ndarray, y: np.ndarray)\
-            -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+            -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         """
         Calculates the properties for Heisenberg Uncertainty Ray Bending.
 
         :param x: ray position at surface, x-coordinate
         :param y: ray position at surface, y-coordinate
-        :return: distances axis 1, distances axis 2, axis 1 vector, axis 2 vector, ray mask for rays to bend
+        :return: distances axis 1, distances axis 2, left-right axis vector, ray mask for rays to bend
         """
         # rotate coordinates to rectangle coordinate system
         x_, y_ = self._rotate_rc(x - self.pos[0], y - self.pos[1], -self._angle)
 
         # see Edge diffraction in Monte Carlo ray tracing Edward R. Freniere, G. Groot Gregory, and Richard A. Hassler
-        a_ = self.dimi[0] / 2 - np.abs(x_)
-        b_ = self.dimi[1] / 2 - np.abs(y_)
+        a_ = self.dimi[1] / 2 - np.abs(y_)
+        b_ = self.dimi[0] / 2 - np.abs(x_)
         inside = (a_ > 0) & (b_ > 0)
         a_, b_ = a_[inside], b_[inside]
 
         # side vector left-right
-        a = np.zeros((a_.shape[0], 3))
-        a[:, 0] = np.cos(self._angle) 
-        a[:, 1] = np.sin(self._angle) 
-        
-        # side vector up-down
-        b = np.zeros_like(a)
-        b[:, 0] = -a[:, 1] 
-        b[:, 1] =  a[:, 0]
+        b = np.zeros((b_.shape[0], 3))
+        b[:, 0] = np.cos(self._angle) 
+        b[:, 1] = np.sin(self._angle) 
 
-        return a_, b_, a, b, inside
+        return a_, b_, b, inside
 
     def mask(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
         """

optrace/tracer/raytracer.py
@@ -421,7 +421,7 @@ class Raytracer(Group):
             raise RuntimeError(f"Ray bending for surface type {type(surf).__name__} not implemented.")
         
         # get bending angles, axes and mask
-        a_, b_, a, b, inside = surf.hurb_props(p[:, i, 0], p[:, i, 1])
+        a_, b_, b, inside = surf.hurb_props(p[:, i, 0], p[:, i, 1])
 
         # std dev of direction gaussian
         # see Edge diffraction in Monte Carlo ray tracing Edward R. Freniere, G. Groot Gregory, and Richard A. Hassler
@@ -434,12 +434,15 @@ class Raytracer(Group):
         tha = scipy.stats.truncnorm.rvs(-sig_th, sig_th, loc=0, scale=sig_a, size=a_.shape[0])
         thb = scipy.stats.truncnorm.rvs(-sig_th, sig_th, loc=0, scale=sig_b, size=b_.shape[0])
 
-        # rotate around a and b for direction variation in b and a
-        sa = misc.rodrigues_rotation(s[inside], a, thb)
-        sab = misc.rodrigues_rotation(sa, b, tha)
-        # TODO actually correct to rotate around a and b?
+        # TODO explain
+        sa = misc.cross(b, s[inside])
+        sa = misc.normalize(sa)
+        sb = misc.cross(s, sa)
 
-        s[inside] = sab
+        # TODO explain
+        sab = s[inside] + sa*np.tan(tha)[:, np.newaxis] + sb*np.tan(thb)[:, np.newaxis]
+        s[inside] = misc.normalize(sab)
+
         # TODO delete s with negative z component -> raytrace warning?
 
 