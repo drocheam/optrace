36854a374f3698d7cfbea69058dc4476cb88123f
Damian Mendroch <damian.mendroch@th-koeln.de>
2026-01-08 14:26:00 +0100

    Fix for MacOS qt vtkmodule import

--- optrace/gui/__init__.py
+++ optrace/gui/__init__.py
@@ -1,1 +1,6 @@
+# fix for MacOS Qt issue
+# see https://gitlab.kitware.com/vtk/vtk/-/issues/19841
+import vtkmodules.qt
+vtkmodules.qt.QVTKRWIBase = "QOpenGLWidget"
+
 from .trace_gui import TraceGUI
