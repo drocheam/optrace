Added scene moving and rotation keyboard shortcuts. Fixed camera skipping issues when interacting with the orientation widget.

optrace/gui/_scene_plotting.py
@@ -249,22 +249,28 @@ class ScenePlotting:
         if self._orientation_axes is not None:
             return
 
-        self._cams = None
+        self.__oa_cam_props = None
 
+        # store camera props and disable render in the case that a handle is selected
         def _on_orientation_change_start(*args):
             self.scene.interactor.EnableRenderOff()
-            self._cams = self.get_camera()[:2]
-
-        # TODO Sprünge wenn y als Normale liegt
-        def _on_orientation_change_end(*args):
-            normal = self.get_camera()[2]
-            roll = 90 if abs(normal[1]) == 1 else 0
-            self.set_camera(*self._cams, roll=roll)
+            self.__oa_cam_props = self.get_camera()
+       
+        # reenable rendering while interacting with the widget
+        def _on_orientation_change_interact(*args):
             self.scene.interactor.EnableRenderOn()
+        
+        # restore camera center and scale and handle +y and -y views so x points up/downwards
+        def _on_orientation_change_end(axes, *args):
+            if axes.GetRepresentation().IsAnyHandleSelected():
+                normal = self.get_camera()[2]
+                roll = 90 if abs(normal[1]) == 1 else 0
+                self.set_camera(*self.__oa_cam_props[:2], roll=roll)
+                self.scene.interactor.EnableRenderOn()
 
         self._orientation_axes = self.scene.add_camera_orientation_widget(animate=False, n_frames=0)
         self._orientation_axes.AddObserver("StartInteractionEvent", _on_orientation_change_start)
-        self._orientation_axes.AddObserver("InteractionEvent", _on_orientation_change_end)
+        self._orientation_axes.AddObserver("InteractionEvent", _on_orientation_change_interact)
         self._orientation_axes.AddObserver("EndInteractionEvent", _on_orientation_change_end)
 
         self._orientation_axes.GetRepresentation().SetSize(90, 90)
@@ -278,7 +284,7 @@ class ScenePlotting:
             # self._plot._orientation.GetRepresentation().SetZAxisColor(0., 0., 0.7)
 
     @staticmethod
-    def calculate_labels(x0, x1, min_s, max_s):
+    def calculate_labels(x0: float, x1: float, min_s: int, max_s: int) -> np.ndarray:
 
         # NOTE min_s and max_s should be in the range 3-50
 
@@ -673,10 +679,9 @@ class ScenePlotting:
 
     def init_keyboard_shortcuts(self) -> None:
         """init keyboard shortcut detection inside the scene"""
-        # TODO pressing arrows moves scene around, pressing with shift+arrows rotates
 
         def keyrelease(vtk_obj, event):
-               
+              
             match vtk_obj.GetKeyCode():
 
                 case "i":  # reset view
@@ -709,13 +714,52 @@ class ScenePlotting:
                     self.scene.render()
 
                 case "-":  # zoom out
-                    self.scene.camera.zoom(0.9)
+                    self.scene.camera.zoom(1/1.1)
+                    self.scene.render()
+                
+                # move camera (no shift) or rotate view (with shift)
+                case _ if (dir_ := vtk_obj.GetKeySym()) in ("Up", "Down", "Left", "Right"):
+                    
+                    cam = self.scene.camera
+                    right = np.cross(cam.GetDirectionOfProjection(), cam.up)
+
+                    if self.scene.interactor.shift_key:
+
+                        if dir_ == "Up":
+                            cam.elevation = cam.elevation + 5
+
+                        elif dir_ == "Down":
+                            cam.elevation = cam.elevation - 5
+
+                        elif dir_ == "Left":
+                            cam.azimuth = cam.azimuth + 5
+
+                        elif dir_ == "Right":
+                            cam.azimuth = cam.azimuth - 5
+
+                    else:
+
+                        if dir_ == "Up":
+                            dvec = np.array(cam.up)*cam.parallel_scale/20
+                        
+                        elif dir_ == "Down":
+                            dvec = -np.array(cam.up)*cam.parallel_scale/20
+                        
+                        elif dir_ == "Right":
+                            dvec = np.array(right)*cam.parallel_scale/15
+                        
+                        elif dir_ == "Left":
+                            dvec = -np.array(right)*cam.parallel_scale/15
+
+                        cam.position = cam.position + dvec
+                        cam.focal_point = cam.focal_point + dvec
+
+                    # recalculate up view and reset camera clipping range
+                    cam.up = np.cross(cam.GetDirectionOfProjection(), -right)
+                    cam.reset_clipping_range()
                     self.scene.render()
 
         # remove default shortcuts from vtk and pyvistaqr
-        # self.scene.iren.remove_observers('CharEvent')
-        # self.scene.iren.remove_observers('KeyPressEvent')
-        # self.scene.iren.remove_observers('KeyReleaseEvent')
         self.scene.iren.interactor.RemoveObservers('CharEvent')
         self.scene.iren.interactor.RemoveObservers('KeyPressEvent')
         self.scene.iren.interactor.RemoveObservers('KeyReleaseEvent')

tests/test_gui.py
@@ -98,7 +98,6 @@ class GUITests(unittest.TestCase):
 
     def click_scene(self, sim, x, y, shift=False, right=False):
 
-        # if shift:
         sim.scene.interactor.SetShiftKey(shift)
         sim.scene.interactor.SetEventPosition(int(x*sim.scene.window_size[0]), int(y*sim.scene.window_size[1]))
         if right:
@@ -724,7 +723,6 @@ class GUITests(unittest.TestCase):
         
         sim.debug(interact, args=(sim,))
    
-    # TODO test zoom
     @pytest.mark.slow
     @pytest.mark.install
     @pytest.mark.gui3
@@ -744,14 +742,15 @@ class GUITests(unittest.TestCase):
 
         # without these high sleeping times there are errors in the github action workflows
 
-        def send_key(sim, key):
+        def send_key(sim, key=None, sym=None):
 
-            def key_press(sim, key):
-                sim.scene.interactor.SetKeyCode(key)
+            def key_press(sim, key, sym):
+                sim.scene.interactor.SetKeyCode(key if key is not None else "\0")
+                sim.scene.interactor.SetKeySym(sym)
                 sim.scene.interactor.InvokeEvent("KeyPressEvent")
                 sim.scene.interactor.InvokeEvent("KeyReleaseEvent")
 
-            self._do_in_main(key_press, sim, key)
+            self._do_in_main(key_press, sim, key, sym)
             self._wait_for_idle(sim)
             time.sleep(0.3)
 
@@ -789,13 +788,57 @@ class GUITests(unittest.TestCase):
                 self.assertFalse(np.allclose(cv, cv2))
                 
                 # maximize scene / hide other menus
-                # self.assertTrue(sim.scene.scene_editor._tool_bar.isVisible())  # TODO
                 self.assertTrue(sim._scene_not_maximized)
                 send_key(sim, "h")
                 self._wait_for_idle(sim)
-                # self.assertFalse(sim.scene.scene_editor._tool_bar.isVisible())
                 self.assertFalse(sim._scene_not_maximized)
+                
+                # zooming in and out
+                sc = sim.scene.camera.parallel_scale
+                send_key(sim, "+")
+                self._wait_for_idle(sim)
+                self.assertTrue(sim.scene.camera.parallel_scale < sc)
+                send_key(sim, "-")
+                self._wait_for_idle(sim)
+                self.assertAlmostEqual(sim.scene.camera.parallel_scale, sc)
 
+                # check scene rotation
+                sim.scene.interactor.shift_key = True
+                el = sim.scene.camera.elevation
+                send_key(sim, sym="Up")
+                self._wait_for_idle(sim)
+                self.assertNotAlmostEqual(sim.scene.camera.elevation, el)
+                send_key(sim, sym="Down")
+                self._wait_for_idle(sim)
+                self.assertAlmostEqual(sim.scene.camera.elevation, el)
+                az = sim.scene.camera.azimuth
+                send_key(sim, sym="Left")
+                self._wait_for_idle(sim)
+                self.assertNotAlmostEqual(sim.scene.camera.azimuth, az)
+                send_key(sim, sym="Right")
+                self._wait_for_idle(sim)
+                self.assertAlmostEqual(sim.scene.camera.azimuth, az)
+                sim.scene.interactor.shift_key = False
+
+                # check scene movement
+                cam_pos = np.array(sim.scene.camera.position)
+                send_key(sim, sym="Up")
+                self._wait_for_idle(sim)
+                self.assertFalse(np.allclose(sim.scene.camera.position, cam_pos))
+                self.assertTrue(cam_pos[1] < sim.scene.camera.position[1]-1e-2)  # move camera down in this yz-view
+                self.assertTrue(np.allclose(cam_pos[::2], np.array(sim.scene.camera.position)[::2])) # others unchanged
+                send_key(sim, sym="Down")
+                self._wait_for_idle(sim)
+                self.assertTrue(np.allclose(sim.scene.camera.position, cam_pos))
+                send_key(sim, sym="Left")
+                self._wait_for_idle(sim)
+                self.assertFalse(np.allclose(sim.scene.camera.position, cam_pos))
+                self.assertTrue(cam_pos[2] > sim.scene.camera.position[2]+1e-2)  # move camera right in this yz-view
+                self.assertTrue(np.allclose(cam_pos[:2], np.array(sim.scene.camera.position)[:2])) # others unchanged
+                send_key(sim, sym="Right")
+                self._wait_for_idle(sim)
+                self.assertTrue(np.allclose(sim.scene.camera.position, cam_pos))
+                
                 # key without shortcut
                 send_key(sim, "w")
                 self._wait_for_idle(sim)